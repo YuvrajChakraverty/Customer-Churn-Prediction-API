e2029e9d2d955ea0b8b6eeb8ecf35d96ac6d6521
YuvrajChakraverty <80749699+YuvrajChakraverty@users.noreply.github.com>
2022-07-13 23:49:48 +0530

    .

--- mainapp.py
+++ mainapp.py
@@ -4,6 +4,23 @@ import pandas as pd
 
 app = Flask(__name__)
 
+def columns_add_remove(data):
+    
+    def tenure_cohort(tenure_months):
+        if tenure_months < 13:
+            return '0-12 Months'
+        elif tenure_months < 25:
+            return '12-24 Months'
+        elif tenure_months < 49:
+            return '24-48 Months'
+        else:
+            return "Over 48 Months"
+    
+    data['Tenure Cohort'] = data['tenure'].apply(tenure_cohort)
+    
+    data=data.drop('customerID',axis=1)
+    
+    return data
 
 @app.route('/', methods=['GET'])
 def ping():
@@ -20,27 +37,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    
-
-    def columns_add_remove(data):
-    
-        def tenure_cohort(tenure_months):
-            if tenure_months < 13:
-                return '0-12 Months'
-            elif tenure_months < 25:
-                return '12-24 Months'
-            elif tenure_months < 49:
-                return '24-48 Months'
-            else:
-                return "Over 48 Months"
-    
-        data['Tenure Cohort'] = data['tenure'].apply(tenure_cohort)
-    
-        data=data.drop('customerID',axis=1)
-    
-        return data
-
     model = joblib.load("Customer Churn Prediction Model.pkl") 
-    col_names = joblib.load("Customer Churn Prediction Columns.pkl") 
-
-    app.run()
+    col_names = joblib.load("Customer Churn Prediction Columns.pkl")
